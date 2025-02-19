e17cc96ebb8ff6f42f63203d3c6de14512301724
etcoder-642 <manassehsamuel642@gmail.com>
2025-02-18 23:55:40 -0800

    - decreased the width and height of the grids - make a trace to appear when a cursor is hovered

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // selecting elements
 const container = document.querySelector(".container");
-const btn = document.querySelector(".btn")
 
 
 // a variable to identify the width and height of the element
@@ -9,18 +8,22 @@ const width = parseInt(styles.width);
 const height = parseInt(styles.height);
 
 // for loop to generate a lot of div elements that will be fitted inside the canvas
-const j = (width*height)/ (32*32)
+const j = (width*height)/ (12*12)
 console.log(j)
+for(i=0;i<j;i++){
+    let div = document.createElement("div")
+    div.style.width = "10px";
+    div.style.height = "10px";
+    div.style.border = "1px solid rgba(0, 0, 0, 0.5)";
+    div.style.padding = '0';
+    div.style.margin = "0"
 
-btn.addEventListener('click', ()=>{
-    for(i=0;i<j;i++){
-        let div = document.createElement("div")
-        div.style.width = "30px";
-        div.style.height = "30px";
-        div.style.border = "1px solid rgba(0, 0, 0, 0.5)";
-        div.style.padding = '0';
-        div.style.margin = "0"
+    container.appendChild(div);
+
+    div.addEventListener('mouseenter', ()=>{
+        div.style.backgroundColor = "black"
+    })
     
-        container.appendChild(div);
-    }    
-})
+}
+
+
