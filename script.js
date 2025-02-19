6aac6872ce0b42f2492aa58dab25d03df72d74db
etcoder-642 <manassehsamuel642@gmail.com>
2025-02-18 23:38:49 -0800

    Basic structure of the grid

---
+++ script.js
@@ -0,0 +1,22 @@
+// selecting elements
+const container = document.querySelector(".container");
+
+
+// a variable to identify the width and height of the element
+const styles = window.getComputedStyle(container);
+const width = parseInt(styles.width);
+const height = parseInt(styles.height);
+
+// for loop to generate a lot of div elements that will be fitted inside the canvas
+const j = (width*height)/ (32*32)
+console.log(j)
+for(i=0;i<j;i++){
+    let div = document.createElement("div")
+    div.style.width = "30px";
+    div.style.height = "30px";
+    div.style.border = "1px solid rgba(0, 0, 0, 0.5)";
+    div.style.padding = '0';
+    div.style.margin = "0"
+
+    container.appendChild(div);
+}
