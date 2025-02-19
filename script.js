3dd2ca89eb044fbcb3363caeb8bad8111e85a1a3
etcoder-642 <manassehsamuel642@gmail.com>
2025-02-19 01:59:05 -0800

    A lot of corrections here are some: - added buttons so users can chose the size of the grid by themselves - added a custom button where they can add a custom grid size - built a mainEngine() function which will generate the grid

--- script.js
+++ script.js
@@ -1,29 +1,73 @@
 // selecting elements
 const container = document.querySelector(".container");
-
-
-// a variable to identify the width and height of the element
-const styles = window.getComputedStyle(container);
-const width = parseInt(styles.width);
-const height = parseInt(styles.height);
+const content = document.querySelector('.content')
 
 // for loop to generate a lot of div elements that will be fitted inside the canvas
-const j = (width*height)/ (12*12)
-console.log(j)
-for(i=0;i<j;i++){
-    let div = document.createElement("div")
-    div.style.width = "10px";
-    div.style.height = "10px";
-    div.style.border = "1px solid rgba(0, 0, 0, 0.5)";
-    div.style.padding = '0';
-    div.style.margin = "0"
 
-    container.appendChild(div);
+document.addEventListener('click',(e)=>{
+    let target = e.target.className
+    if(target.includes('first-child')){ 
+        mainEngine(16);
+    }else if(target.includes('second-child')){
+        mainEngine(32);        
+    }else if(target.includes('third-child')){
+        mainEngine(64);
+    }else if(target.includes('custom')){
+        content.innerHTML = "";
+        let customInput = document.createElement('input')
+        let customButton = document.createElement('button')
+        customButton.textContent = "Set"
 
-    div.addEventListener('mouseenter', ()=>{
-        div.style.backgroundColor = "black"
-    })
+        content.appendChild(customInput)
+        content.appendChild(customButton)
+        customInput.focus()
+
+        customInput.setAttribute('type', 'number')
+        customInput.setAttribute('placeholder', 'Enter a Number')
+        customButton.addEventListener('click', ()=>{
+            if(typeof (customInput.value) != 'number'){
+                alert("You haven't entered a number.")
+            }else if(parseInt(customInput.value)>64){
+                alert("This is too big! Choose a smaller one!")
+            }else{
+                mainEngine(parseInt(customInput.value))
+            }
+        })
+    }
+})
+
+function mainEngine(value) {
+    // clearing the ground before starting the actual work
+    container.innerHTML = '';
+
+    // sets the width and height of the grid
+    let f = 16*value
+    container.style.width = `${f}px`
+    container.style.height = `${f}px`
+ 
+    // a variable to identify the width and height of the element
+    let styles = window.getComputedStyle(container);
+    let width = parseInt(styles.width);
+    console.log(width + ' - this is the width')
+    let height = parseInt(styles.height);
+
+    // generating the grid
+    let j = (width*height)/ (16*16)
+    console.log(j + ' - this is j')
+    for(i=0;i<j;i++){
+        let div = document.createElement("div")
+        div.style.width = "14px";
+        div.style.height = "14px";
+        div.style.border = "1px solid rgba(0, 0, 0, 0.5)";
+        div.style.padding = '0';
+        div.style.margin = "0"
     
-}
+        container.appendChild(div);
 
+        // making the trace happen when hovered    
+        div.addEventListener('mouseenter', ()=>{
+            div.style.backgroundColor = "black"
+        })
+    }
+}
 
